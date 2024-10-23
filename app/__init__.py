bfe70b646612fb6db773cfbfb2e9487c5c47d34a
Developer.CQ <developer.cq.colombia@gmail.com>
2024-10-23 17:39:31 -0500

    new files

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration settings (e.g., from config.py)
+    app.config.from_object('config.Config')
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints
+    from app.routes.user_routes import user_routes
+    app.register_blueprint(user_routes)
+
+    return app
